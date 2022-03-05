6403af4b9195cb73983942fce8db0b7f4077ff19
Fayziddin2000 <boriyevfayz@gmail.com>
2022-03-05 19:33:10 +0500

    -created

--- app/src/main/java/com/boriyevfayz/my1/MainActivity.kt
+++ app/src/main/java/com/boriyevfayz/my1/MainActivity.kt
@@ -1,237 +1,112 @@
 package com.boriyevfayz.my1
 
 import android.os.Bundle
-import android.text.Editable
+import android.util.Log
 import android.view.View
-import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.boriyevfayz.my1.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var _biding: ActivityMainBinding
-    private val mBinding get() = _biding
-
-    private var canAddOperation =false
-    private var canAddDemical = true
+    private lateinit var _binding: ActivityMainBinding
+    private val mBinding: ActivityMainBinding get() = _binding
+    private var input: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _biding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
-
-
-
-    }
-
-    fun zeroBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
+        input = mBinding.tvWorking
+        mBinding.clear.setOnClickListener {
+            mBinding.tvWorking.text = ""
+            mBinding.tvResult.text = ""
         }
-    }
-
-
-
-    fun oneBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
-        }
-    }
-
-    fun twoBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
-        }
-    }
-
-    fun threeBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
-        }
-    }
-
-    fun fourBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
-        }
-    }
-
-    fun fiveBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
-        }
-    }
-
-    fun sixBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
-        }
-    }
-
-    fun sevenBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
-        }
-    }
-
-    fun eightBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
-        }
-    }
-
-    fun nineBtn(view: View?) {
-        if (view is Button)
-        {
-            workingtv.append(view.text)
-            canAddOperation = true
-        }
-    }
-
-    fun plusBtn(view: View?) {
-        if (view is Button && canAddOperation)
-        {
-            workingtv.append(view.text)
-            canAddOperation = false
-            canAddDemical = true
-        }
-    }
-
-    fun minusBtn(view: View?) {
+        initNumPad()
 
     }
 
-    fun multiplyBtn(view: View?) {
+    private fun initNumPad() {
+        mBinding.cvNum0.setOnClickListener(this)
+        mBinding.cvNum1.setOnClickListener(this)
+        mBinding.cvNum2.setOnClickListener(this)
+        mBinding.cvNum3.setOnClickListener(this)
+        mBinding.cvNum4.setOnClickListener(this)
+        mBinding.cvNum5.setOnClickListener(this)
+        mBinding.cvNum6.setOnClickListener(this)
+        mBinding.cvNum7.setOnClickListener(this)
+        mBinding.cvNum8.setOnClickListener(this)
+        mBinding.cvNum9.setOnClickListener(this)
+        mBinding.cvPoint.setOnClickListener(this)
+    }
+
+    private fun sum() {
 
     }
 
-    fun divideBtn(view: View?) {
-
+    private fun getNumb() {
+        val nums = mBinding.tvWorking.text.split("")
+        Log.d("TAG", "getNums: $nums ")
     }
 
-    fun percentBtn(view: View?) {
-
+    private fun result() {
     }
 
-    fun clearBtn(view: View) {
 
-        mBinding.workingtv.text = "" as Editable?
-        mBinding.answertv.text = "" as Editable?
-    }
+    override fun onClick(view: View) {
 
-    fun bracketsBtn(view: View) {
-        val length = mBinding.workingtv.length()
-        if(length > 0)
-            mBinding.workingtv.text = mBinding.workingtv.text.subSequence(0,length - 1) as Editable?
-    }
-
-    fun pointBtn(view: View?) {
-        if (canAddDemical)
-            mBinding.workingtv.append(view.text)
-        canAddDemical
-
-    }
-
-    fun equalBtn(view: View?) {
-        mBinding.answertv.text = calculateResults()
-    }
-
-    private fun calculateResults(): String {
-        val digitsOperators = digitsOperators()
-        if(digitsOperators.isEmpty()) return ""
-        val timesDivision = timesDivisionCalculate(digitsOperators)
-        if(timesDivision.isEmpty()) return ""
-        val result = addSubtractCalculate(timesDivision)
-        return result.toString()
-    }
-
-    private fun addSubtractCalculate(passedList: MutableList<Any>):Float {
-        var result = passedList[0] as Float
-        for(i in passedList.indices){
-            if(passedList[i] is Char && i!= passedList.lastIndex)
-            {
-                val operator = passedList[i]
-                val nextDigit = passedList[i+1] as Float
-                if(operator == '+')
-                    result += nextDigit
-                if(operator == '-')
-                    result -= nextDigit
-            }
-
-        }
-        return result
-    }
-
-    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {
-        var list =passedList
-        while (list.contains('x')||list.contains("/")){
-            list = calcTimesDiv(list)
-        }
-        return list
-    }
-
-    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any> {
-        val  newList = mutableListOf<Any>()
-        var restartIndex = passedList.size
-        for(i in passedList.indices){
-            if (passedList[i] is Char && i!=passedList.lastIndex && i<restartIndex){
-                val operator = passedList[i]
-                val prevDigit = passedList[i-1] as Float
-                val nextDigit = passedList[i+1] as Float
-                when(operator){
-                    'x'->{
-                        newList.add(prevDigit * nextDigit)
-                        restartIndex = i + 1
+        when (view.id) {
+            mBinding.cvNum0.id -> {
+                when {
+                    input!!.text.isNullOrEmpty() -> input!!.text = "0"
+                    input!!.text[0] == '0' -> {
                     }
-                    '/'->{
-                        newList.add(prevDigit / nextDigit)
-                        restartIndex = i + 1
-                    }
-                    else->{
-                        newList.add(prevDigit)
-                        newList.add(operator)
+                    else -> {
+                        val tmp = StringBuilder()
+                        tmp.append(input!!.text)
+                        tmp.append(0)
+                        input!!.text = tmp.toString()
                     }
                 }
-                if (i>restartIndex)
-                    newList.add(passedList[i])
-
             }
-        }
-        return newList
-    }
-
-
-    fun pamBtn(view: View?) {
-
-    }
-
-    fun backspaceBtn(view: View?) {
-
-    }
-    private fun digitsOperators(): MutableList<Any>{
-        val list = mutableListOf<Any>()
-        var currentDigit = ""
-        for(character in workingtv.text){
-            if (character.isDigit() || character == ".")
-                currentDigit += character
-            else {
-                list.add(currentDigit.toFloat())
-                currentDigit = ""
-                list.add(character)
+            mBinding.cvNum1.id -> pastNumb(1.toString(), input!!)
+            mBinding.cvNum2.id -> pastNumb(2.toString(), input!!)
+            mBinding.cvNum3.id -> pastNumb(3.toString(), input!!)
+            mBinding.cvNum4.id -> pastNumb(4.toString(), input!!)
+            mBinding.cvNum5.id -> pastNumb(5.toString(), input!!)
+            mBinding.cvNum6.id -> pastNumb(6.toString(), input!!)
+            mBinding.cvNum7.id -> pastNumb(7.toString(), input!!)
+            mBinding.cvNum8.id -> pastNumb(8.toString(), input!!)
+            mBinding.cvNum9.id -> pastNumb(9.toString(), input!!)
+            mBinding.cvPoint.id -> {
+                if (input!!.text.isNullOrEmpty()) input!!.text = "0."
+                else if (input!!.text[0] == '0') {
+                    input!!.text = "0."
+                } else {
+                    val tmp = StringBuilder()
+                    tmp.append(input!!.text)
+                    tmp.append('.')
+                    input!!.text = tmp.toString()
+                }
             }
+            mBinding.cvPlus.id -> {
+                pasteSymbol(resources.getString(R.string.sum), input)
+            }
+
         }
-        if(currentDigit != "")
-            list.add(currentDigit.toFloat())
-        return list
+
     }
+
+    private fun pastNumb(num: String, input: TextView) {
+        if (input.text.isNullOrEmpty()) input.text = num
+        else if ((input.text[0] == '0' && input.text[1] != '.') || (input.text[0] == '-' && input.text[0] != '0' && input.text[2] != '.')) {
+            input.text = num
+        } else {
+            val tmp = StringBuilder()
+            tmp.append(input.text)
+            tmp.append(num)
+            input.text = tmp.toString()
+        }
+    }
+
 }
